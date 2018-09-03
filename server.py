945456ed2f639a98417830bd1b384cb363d716db
ZSSE <zsse@iMacZSSE2.local>
2018-09-03 12:06:16 +0600

    initial commit

---
+++ server.py
@@ -0,0 +1,37 @@
+import time
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import os
+
+
+HOST_NAME = 'localhost' # !!!REMEMBER TO CHANGE THIS!!!
+PORT_NUMBER = 9000 # Maybe set this to 9000.
+
+
+class MyHandler(BaseHTTPRequestHandler):
+    def do_HEAD(s):
+        s.send_response(200)
+        s.send_header("Content-type", "text/html")
+        s.end_headers()
+    def do_GET(s):
+        """Respond to a GET request."""
+        s.send_response(200)
+        s.send_header("Content-type", "text/html")
+        s.end_headers()
+        s.wfile.write("<html><head><title>Title goes here.</title></head>".encode())
+        s.wfile.write("<body><p>This is a test.</p>".encode())
+        # If someone went to "http://something.somewhere.net/foo/bar/",
+        # then s.path equals "/foo/bar/".
+        s.wfile.write("<p>You accessed path: %s</p>".encode() % s.path)
+        s.wfile.write("</body></html>".encode())
+
+if __name__ == '__main__':
+    server_class = HTTPServer
+    httpd = server_class((HOST_NAME, PORT_NUMBER), MyHandler)
+    print (time.asctime(), "Server Starts - %s:%s" % (HOST_NAME, PORT_NUMBER))
+    print (os.system("date"))
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    print (time.asctime(), "Server Stops - %s:%s" % (HOST_NAME, PORT_NUMBER))
