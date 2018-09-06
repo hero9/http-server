31e08e8a0d0c38be096d54aedba77e6521721505
ZSSE <zsse@iMacZSSE2.local>
2018-09-06 18:07:59 +0600

    commit

--- baseHttpServer.py
+++ baseHttpServer.py
@@ -1,10 +1,9 @@
-from PIL import Image, ImageDraw, ImageFont
-image = Image.open('msg.jpg')
-draw = ImageDraw.Draw(image)
-font = ImageFont.truetype('Roboto-Bold.ttf', size=13)
-(x, y) = (50, 5)
-name = 'Build: Success'
-color = '#b85c00' # white color
-draw.text((x, y), name, fill=color, font=font)
-
-image.save('msg1.png')
+server {
+        server_name 207.154.246.33;
+        root /var/www;
+        index index.html;
+        location / {
+                try_files $uri =404;
+                expires -1;
+        }
+}
--- http-server.py
+++ http-server.py
@@ -1,16 +1,38 @@
 from flask import Flask, request
 import json
+import subprocess
+import os
+from PIL import Image, ImageDraw, ImageFont
 
 app = Flask(__name__)
 
 @app.route('/',methods=['POST'])
 def foo():
   data = json.loads(request.data)
-  # print ("New commit by: {}".format(data['commits'][0]['author']['name']))
   if data:
-    print (data)
+    print ("hello")
+    bashCommand = """
+    if git clone https://github.com/influxdata/chronof.git ; then
+        echo "Command succeeded"
+    else
+        echo "Command failed"
+    fi
+    """
+    os.system(bashCommand)
+    # os.chdir("./git/http-server")
+    # subprocess.call(["git", "pull", "origin", "master"])
+    # os.chdir("../../") 
+    image = Image.open('msg.jpg')
+    draw = ImageDraw.Draw(image)
+    font = ImageFont.truetype('Roboto-Bold.ttf', size=13)
+    (x, y) = (50, 5)
+    name = 'Build: Success'
+    color = '#b85c00'
+    draw.text((x, y), name, fill=color, font=font)
+    # os.chdir("/var/www")
+    image.save('msg1.png')
     return "OK"
   return "NOT OK"
 
 if __name__ == '__main__':
-   app.run(debug=True, host="127.0.0.1", port=8898)
+   app.run(host="localhost", port=8888)
