75715be86d1f499e97275be8b7a1877af527f13b
ZSSE <zsse@iMacZSSE2.local>
2018-09-10 10:04:44 +0600

    fully ready

--- README.md
+++ README.md
@@ -3,3 +3,16 @@
 python 3.6
 # continuos integration
 ready!
+
+# nginx_config
+```
+server {
+        server_name 207.154.246.33;
+        root /var/www;
+        index index.html;
+        location / {
+                try_files $uri =404;
+                expires -1;
+        }
+}
+```
--- http-server.py
+++ http-server.py
@@ -9,10 +9,10 @@ app = Flask(__name__)
 
 @app.route('/',methods=['POST'])
 def foo():
-  data = json.loads(request.data)
+  # data = json.loads(request.data)
   os.chdir("chronograf/") # Choose the path to project root
   subprocess.call(["make", "clean"])
-  subprocess.call(["git", "pull", "origin", "chrono-predix-zsse"])
+  subprocess.call(["git", "pull", "origin", "master"]) # chrono-predix-zsse
   result = subprocess.call(["make"])
   if result == 0:
     name = "Build: Succeed"
@@ -20,18 +20,39 @@ def foo():
   else:
     name = "Build: Failed"
     color = 'red'
-  subprocess.call(["chmod", "+x", "etc/fix-folder.sh"])
-  subprocess.call(["sh", "etc/fix-folder.sh"])
+
+  # subprocess.call(["chmod", "+x", "etc/fix-folder.sh"])
+  # subprocess.call(["sh", "etc/fix-folder.sh"])
+  os.chdir("ui/build")
+  subprocess.call(["ln", "-s", "../bower_components"]) # fix fix_folder.sh script
+  subprocess.call(["ln", "-s", "../static_assets"]) #fix fix_folder.sh script
+  os.chdir("../../")
+
   Popen(["make", "run"], shell=True, stdin=None, stdout=None, stderr=None, close_fds=True)
-  os.chdir("../")
+
+  os.chdir("../") # to http-server.py location
+
   image = Image.open('msg.png')
   draw = ImageDraw.Draw(image)
-  font = ImageFont.truetype('Roboto-BoldItalic.ttf', size=16)
-  (x, y) = (45, 6)
+  font = ImageFont.truetype('Roboto-BoldItalic.ttf', size=13)
+  
+  (x, y) = (40, 5)
   draw.text((x, y), name, fill=color, font=font)
-  # os.chdir("/var/www") # Choose path to where you want to save output picture
+  
+  with open('chronograf/ui/package.json') as package_json:
+    data = json.load(package_json)
+    name = data['version']
+  
+  (x, y) = (110, 15)
+  font = ImageFont.truetype('Roboto-BoldItalic.ttf', size=10)
+  color = 'white'
+  draw.text((x, y), name, fill=color, font=font)
+  
+  os.chdir("/var/www") # Choose path where you want to save output picture
+  
   image.save('result.png')
+  
   return "OK"
 
 if __name__ == '__main__':
-   app.run(host="localhost", port=8181)
+   app.run(host="207.154.246.33", port=8181) # type name or IP_address of your server and port
